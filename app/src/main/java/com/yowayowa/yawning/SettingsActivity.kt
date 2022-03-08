3acdd166d89f3f18c267a783dc3bf47bdc9f5e7d
Atria <atriasft@gmail.com>
2022-03-09 05:01:31 +0900

    Implement SettingsActivity

--- app/src/main/java/com/yowayowa/yawning/SettingsActivity.kt
+++ app/src/main/java/com/yowayowa/yawning/SettingsActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.widget.Toolbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentContainerView
 
 class SettingsActivity : AppCompatActivity(R.layout.activity_settings){
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,6 +13,10 @@ class SettingsActivity : AppCompatActivity(R.layout.activity_settings){
         toolbar.title = "設定"
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        supportFragmentManager.beginTransaction().setReorderingAllowed(true)
+            .add(R.id.fragmentContainerView,SettingsFragment::class.java,null)
+            .commit()
     }
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         if (item.itemId == android.R.id.home) finish()
