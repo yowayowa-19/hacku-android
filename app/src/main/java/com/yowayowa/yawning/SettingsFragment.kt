1e68af7508426dc082dc6dfb8c90458fe29c754e
Atria <atriasft@gmail.com>
2022-03-09 05:13:27 +0900

    Auto drawing version info in Settings

--- app/src/main/java/com/yowayowa/yawning/SettingsFragment.kt
+++ app/src/main/java/com/yowayowa/yawning/SettingsFragment.kt
@@ -2,9 +2,12 @@ package com.yowayowa.yawning
 
 import android.os.Bundle
 import androidx.preference.PreferenceFragmentCompat
+import androidx.preference.PreferenceScreen
 
 class SettingsFragment : PreferenceFragmentCompat() {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         setPreferencesFromResource(R.xml.preferences, rootKey)
+        val versionPreferenceScreen = findPreference<PreferenceScreen>("version")
+        versionPreferenceScreen?.summary = BuildConfig.VERSION_NAME
     }
 }
