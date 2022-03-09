0cbf19b8561b4077a9d3650143788b5de6fec1cc
Atria <atriasft@gmail.com>
2022-03-10 00:59:25 +0900

    Implement addMarker

--- app/src/main/java/com/yowayowa/yawning/MapActivity.kt
+++ app/src/main/java/com/yowayowa/yawning/MapActivity.kt
@@ -1,12 +1,18 @@
 package com.yowayowa.yawning
 
+import android.graphics.Color
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import androidx.core.graphics.BlendModeColorFilterCompat
+import androidx.core.graphics.BlendModeCompat
 import com.yowayowa.yawning.databinding.ActivityMapBinding
 import org.osmdroid.config.Configuration
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory
 import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
 
 
 class MapActivity : AppCompatActivity() {
@@ -31,5 +37,17 @@ class MapActivity : AppCompatActivity() {
         val mapController = binding.map.controller
         mapController.setZoom(9.0)
         mapController.setCenter(jpZeroPoint)
+        addMarker(map,jpZeroPoint, Color.BLUE)
+    }
+    private fun addMarker(map:MapView ,geoPoint: GeoPoint,color:Int){
+        val marker = Marker(map)
+        marker.position = geoPoint
+        val icon = ResourcesCompat.getDrawable(resources,R.drawable.ic_baseline_adjust_24,null)
+        icon?.colorFilter = BlendModeColorFilterCompat.createBlendModeColorFilterCompat(color,
+            BlendModeCompat.SRC_ATOP)
+        marker.icon = icon
+        marker.setAnchor(Marker.ANCHOR_CENTER,Marker.ANCHOR_CENTER)
+        map.overlays.add(marker)
     }
 }
+
