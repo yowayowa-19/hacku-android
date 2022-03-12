467eee04286d8a94a99ede3304c0d30cdf749c7c
Atria <atriasft@gmail.com>
2022-03-12 10:17:38 +0900

    implement dummy

--- app/src/main/java/com/yowayowa/yawning/ui/ranking/RankingViewModel.kt
+++ app/src/main/java/com/yowayowa/yawning/ui/ranking/RankingViewModel.kt
@@ -16,10 +16,17 @@ class RankingViewModel : ViewModel() {
         return comboRankings
     }
     private fun loadComboRanking(): List<ComboRanking> {
-        val ria = ComboRanking(1, 3, "riaa")
-        val yuhi = ComboRanking(2, 2, "yuhi")
-        val awa = ComboRanking(3, 1, "awa")
-        return listOf(ria, yuhi, awa)
+        val user1 = ComboRanking(1, 27, "user1")
+        val user2 = ComboRanking(2, 22, "user2")
+        val user3 = ComboRanking(3, 21, "user3")
+        val user4 = ComboRanking(4, 21, "user4")
+        val user5 = ComboRanking(5, 14, "user5")
+        val user6 = ComboRanking(6, 12, "user6")
+        val user7 = ComboRanking(7, 10, "user7")
+        val user8 = ComboRanking(8, 5, "user8")
+        val user9 = ComboRanking(9, 3, "user9")
+        val user10 = ComboRanking(10, 3, "user10")
+        return listOf(user1, user2, user3, user4, user5, user6, user7, user8, user9, user10)
     }
     private val distanceRankings:MutableLiveData<List<DistanceRanking>> by lazy{
         MutableLiveData<List<DistanceRanking>>().also {
@@ -30,9 +37,16 @@ class RankingViewModel : ViewModel() {
         return distanceRankings
     }
     private fun loadDistanceRanking(): List<DistanceRanking> {
-        val ria = DistanceRanking(1, 2.425, "riaa")
-        val yuhi = DistanceRanking(2, 2.1, "yuhi")
-        val awa = DistanceRanking(3, 1.75, "awa")
-        return listOf(ria, yuhi, awa)
+        val user1 = DistanceRanking(1, 27.42, "user1")
+        val user2 = DistanceRanking(2, 25.1, "user2")
+        val user3 = DistanceRanking(3, 24.75, "user3")
+        val user4 = DistanceRanking(4, 21.15, "user4")
+        val user5 = DistanceRanking(5, 17.12, "user5")
+        val user6 = DistanceRanking(6, 16.8, "user6")
+        val user7 = DistanceRanking(7, 15.2, "user7")
+        val user8 = DistanceRanking(8, 5.1, "user8")
+        val user9 = DistanceRanking(9, 3.5, "user9")
+        val user10 = DistanceRanking(10, 3.2, "user10")
+        return listOf(user1, user2, user3, user4, user5, user6, user7, user8, user9, user10)
     }
 }
